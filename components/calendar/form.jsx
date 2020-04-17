d3a6d5153b3f0aa92ba2b3d4214ef9a7bb734868
jhohloch <jannik.hohloch@outlook.de>
2020-04-17 14:51:00 +0200

    Lightbox+ Formmethoden deklariert

--- components/calendar/form.jsx
+++ components/calendar/form.jsx
@@ -1,39 +1,73 @@
 import React from 'react';
 import styles from './form.module.css';
+import axios from 'axios';
+import AppContext from '../appContext';
 
 export default class Form extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      title:'',
+      time1:'',
+      time2:'',
+      //time: time1+ '-'+ time2,
+      room:'',
+      message:'',
+    };
+    this.changeTitle = this.changeTitle.bind(this);
+    this.changeTime1 = this.changeTime1.bind(this);
+    this.changeTime2 = this.changeTime2.bind(this);
+    this.changeRoom = this.changeRoom.bind(this);
   }
+  changeTitle(event) {
+    this.setState({title: event.target.value});
+  }
+  changeTime1(event) {
+    this.setState({time1: event.target.value});
+  }
+  changeTime2(event) {
+    this.setState({time2: event.target.value});
+  }
+  changeRoom(event) {
+    this.setState({room: event.target.value});
+  }
+
+  //async submitForm(event) {
+    //event.preventDefault();
+    //this.setState({message: ''});
+    //try {
+      //await axios.post
+    //}
+  //}
 
   render() {
     return (
       <div className={styles.lightbox}>
         <div className={styles.createLectureForm}>
           <button onClick={this.props.onClose}>Close</button>
-          <form>
+          <form onSubmit={this.submitForm}>
             <label>
               Title:
                 <br />
-              <input type="text" required />
+              <input type="text" required value={this.state.title} onChange={this.changeTitle} />
             </label>
             <br />
             <label>
               Time:
                 <br />
               <span>
-                <input type="time" />
+                <input type="time" value={this.state.time1} onChange={this.changeTime1} />
                   -
-                  <input type="time" />
+                  <input type="time" value={this.state.time2} onChange={this.changeTime2}/>
               </span>
             </label>
             <br />
             <label>
               Room:
                 <br />
-              <input type="text" />
+              <input type="text" value={this.state.room} onChange={this.changeRoom}/>
             </label>
-            <button type="submit">Create</button>
+            <button type="submit" onClick={this.props.onSubmit}>Create</button>
           </form>
         </div>
       </div>
--- components/calendar/month.jsx
+++ components/calendar/month.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import styles from './month.module.css';
+import Calendar from '../calendar';
+import Form from './form';
+
 
 Date.prototype.getMonthDays = function () {
   const d = new Date(this.getFullYear(), this.getMonth() + 1, 0);
   return d.getDate();
 };
 
+function Popup(props) {
+  return (
+    <button
+    className = "popup"
+    onClick = {props.onClick}
+    >
+      {props.showPopup}
+    </button>
+  );
+}
+
 export default class Month extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       date: new Date(),
+
+
     };
     this.previousMonth = this.previousMonth.bind(this);
     this.nextMonth = this.nextMonth.bind(this);
+
   }
 
   previousMonth() {
@@ -35,14 +52,18 @@ export default class Month extends React.Component {
     }
     this.setState({ date: nextMonth });
   }
+  
+  
 
   render() {
     const days = [];
+    
     for (let d = 1; d <= this.state.date.getMonthDays(); d++) {
-      days.push(<Day key={d} number={d} />);
+      days.push(<Day key={d} number={d}  />);
     }
     return (
       <>
+        
         <div className={styles.monthHeader}>
           <button onClick={this.previousMonth}>previous</button>
           {this.state.date.toDateString()}
@@ -51,19 +72,69 @@ export default class Month extends React.Component {
         <div className={styles.month}>
           {days}
         </div>
+       
       </>
     );
   }
 }
 
-class Day extends React.Component {
+export class Day extends React.Component {
   constructor(props) {
     super(props);
+    this.state = 
+    {
+      showPopup:false,
+      closed: false
+    };
+    this.toggleShowPopup=this.toggleShowPopup.bind(this)
+    this.handleOutsideClick=this.handleOutsideClick.bind(this)
+  
   }
 
+  
+
+ toggleShowPopup() {
+   if(!this.state.showPopup) {
+     document.addEventListener('click', this.handleOutsideClick, false)
+   }
+   else {
+     document.removeEventListener('click', this.handleOutsideClick, false)
+   }
+
+   this.setState({
+     showPopup: !this.state.showPopup
+   });
+ }
+ handleOutsideClick(e) {
+   if(this.node.contains(e.target)) {
+     return;
+   }
+   this.toggleShowPopup();
+ }
+ handleSubmit() {
+
+ }
+
   render() {
+    
     return (
-      <div className={styles.monthDay}>{this.props.number}</div>
+      <div>
+      
+      <div  
+      className={styles.monthDay}
+      ref={node => { this.node = node; }} 
+      onClick={() =>{this.toggleShowPopup()}}
+      showPopup={this.state.showPopup} 
+       >
+         {this.props.number}
+         </div>
+
+         {this.state.showPopup ? <Form 
+         onClose={this.toggleShowPopup.bind(this)}
+      
+         /> : null }
+      </div>
+      
     );
   }
 }
